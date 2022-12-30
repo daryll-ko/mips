b3f74b2b1147ca9d0ce878299c531fc6a59573a3
Daryll Ko <daryll.ko@gmail.com>
2022-12-30 19:06:36 +0800

    refactor: remove unnecessary variables

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ mips = Mips()
 program = []
 
 with open("input.txt", 'r') as input_file:
-    lines = input_file.readlines()
-    for line in lines:
-        program.append(int(line, 16))
+    for line in input_file.readlines():
+        program.append(int(line.strip(), 16))
 
 mips.load_program(program)
 mips.run_program()
